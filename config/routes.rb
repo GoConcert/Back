0835cdc171ad6b42e9708f046279cf387763cb07
EtiMrc <etienne.marecal@outlook.fr>
2016-10-28 13:01:00 +0200

    search

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Rails.application.routes.draw do
   resources :user_preferences
   resources :reservations
   resources :concert_preferences
+
   get 'home/index'
   root 'home#index'
   post 'concerts/:id/reservation' => 'concerts#reservation'
 
-  #get 'concerts/search.json?location=Paris' => 'concerts#search'
-  get 'search/:location' => 'concerts#search'
-  get 'concerts/search.json?location=:location' => 'concerts#search'
+  get 'concerts/search/:location' => 'concerts#search'
+  #get 'concerts/search.json?location=:location' => 'concerts#search'
+  get 'concerts/search.json' => 'concerts#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
